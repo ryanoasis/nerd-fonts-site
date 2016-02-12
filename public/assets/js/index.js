435c699f6c82d147df4bf5c4bd5ec14a0a7a6dbe
Ryan McIntyre <rmcintyre@acronym.com>
2016-02-12 18:54:42 -0500

    Lots of tweaks and changes to make the UI look at least respectable/presentable

---
+++ public/assets/js/index.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+	 $("[name='mono']").bootstrapSwitch({ inverse: true, size: 'mini', onText: 'Single', offText: 'Double', onColor: 'primary', offColor: 'primary' });
+	 $("[name='windows']").bootstrapSwitch({ size: 'mini', onText: 'Yes', offText: 'No', onColor: 'warning', offColor: 'success' });
+});
